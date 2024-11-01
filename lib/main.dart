c587b346e514f569dd068257e92221f1f504928e
shahd <shoshomohy13@gmail.com>
2024-11-01 13:53:20 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotsApp());
+}
+
+class NotsApp extends StatelessWidget {
+  const NotsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
